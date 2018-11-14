78548f5bdc3a47a87019ab38c01980422306bcd8
sw-yx <swyx@swyx.io>
2018-11-14 02:09:33 -0800

    ughghgh

--- src/lambda/hosts.js
+++ src/lambda/hosts.js
@@ -4,7 +4,8 @@ export function handler(event, context, callback) {
   // console.log('queryStringParameters', event.queryStringParameters);
   // const db = fs.readFileSync(path.resolve('../db.json'));
   // const db = fs.readFileSync('../db.json');
-  const db = fs.readFileSync(path.resolve('db.json'));
+  // const db = fs.readFileSync(path.resolve('db.json'));
+  const db = fs.readFileSync(path.join(__dirname, 'db.json'));
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(JSON.parse(db).hosts)
