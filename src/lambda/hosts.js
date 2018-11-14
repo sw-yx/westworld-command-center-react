521b73283ca960b7a4f4ff35ee9a028160c2a917
sw-yx <swyx@swyx.io>
2018-11-14 01:50:05 -0800

    path?

--- src/lambda/hosts.js
+++ src/lambda/hosts.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 export function handler(event, context, callback) {
   // console.log('queryStringParameters', event.queryStringParameters);
-  const db = fs.readFileSync(path.resolve('./db.json'));
+  const db = fs.readFileSync(path.resolve('../db.json'));
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(JSON.parse(db).hosts)
