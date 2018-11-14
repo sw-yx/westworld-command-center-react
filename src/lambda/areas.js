9c3a137017742465e73218164a01dc81778587f7
sw-yx <swyx@swyx.io>
2018-11-14 02:05:44 -0800

    retry

--- src/lambda/areas.js
+++ src/lambda/areas.js
@@ -3,7 +3,7 @@ const path = require('path');
 export function handler(event, context, callback) {
   // console.log('queryStringParameters', event.queryStringParameters);
   // const db = fs.readFileSync(path.resolve('../db.json'));
-  const db = fs.readFileSync('./db.json');
+  const db = fs.readFileSync(path.resolve('db.json'));
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(JSON.parse(db).areas)
